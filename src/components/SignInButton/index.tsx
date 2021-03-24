2d7d9ce7ca9ae974ad033c147348d9274356f535
Bruno Sonego <sonego.bruno@gmail.com>
2021-03-24 19:05:36 -0300

    Adicionado criaçao de clientes no stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,27 @@
+import { signIn, useSession, signOut } from 'next-auth/client'
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
-    const isUserLoggedIn = true;
+
 export function SignInButton() {
-    return isUserLoggedIn ? (
+    const [ session ] = useSession();
+
+    return session ? (
         <button 
-        type="button"
-        className={styles.signInButton}
-    >
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
         <FaGithub color="#04d361"/>
-        Bruno Sônego
+        {session.user.name}
         <FiX color="#737380" className={styles.rightIcon}/>
     </button>
     ) : (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417"/>
             Sign in with Github
---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,31 @@
+import { signIn, useSession } from 'next-auth/client';
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
+    priceId: string
+}
+
+export function SubscribeButton({
+    priceId
+}: SubscribeButtonProps) {
+    const [session] = useSession()
+    
+    function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return
+        }
+
+        //Criação checkout session
+        
+    }
+
+    return (
+        <button
+            type="button"
+            className={styles.subscribeButton}
+        >
+            Subscribe now
+        </button>
+    );
+}
